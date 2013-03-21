2e6ad1a7e99e679c7335a4ea639073a562afe24d
Bjørge Næss <bjoerge@bengler.no>
2013-03-21 17:58:39 +0100

    Emit sync and error events

--- pebbles-backbone.js
+++ pebbles-backbone.js
@@ -24,6 +24,19 @@ exports.pebblify = function (backboneClass) {
           }
           var url = typeof model.url === 'function' ? model.url() : model.url;
           var promise = opts.service.perform(methodMap[method], url, options.data, headers);
+
+          var success = options.success;
+          options.success = function(resp, status, xhr) {
+            if (success) success(resp, status, xhr);
+            model.trigger('sync', model, resp, options);
+          };
+
+          var error = options.error;
+          options.error = function(xhr, status, thrown) {
+            if (error) error(model, xhr, options);
+            model.trigger('error', model, xhr, options);
+          };
+
           promise.then(options.success, options.error);
           return promise;
         };
