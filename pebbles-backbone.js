22ca180a5e492da20b04cff84802474c64402707
Bjørge Næss <bjoerge@bengler.no>
2013-08-06 11:19:44 +0200

    Don't emit 'sync' and 'error' events from the overridden sync method

--- pebbles-backbone.js
+++ pebbles-backbone.js
@@ -29,13 +29,11 @@ exports.pebblify = function (backboneClass) {
           var success = options.success;
           options.success = function(resp, status, xhr) {
             if (success) success(resp, status, xhr);
-            model.trigger('sync', model, resp, options);
           };
 
           var error = options.error;
           options.error = function(xhr, status, thrown) {
             if (error) error(model, xhr, options);
-            model.trigger('error', model, xhr, options);
           };
 
           promise.then(options.success, options.error);
